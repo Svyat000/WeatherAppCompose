e80105f1fec6a99ab98f9e15d24aff9fa8c77316
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-04-14 21:40:06 +0300

    ref TabLayout add dependencies foundation

--- app/src/main/java/com/sddrozdov/weatherappcompose/ui/screens/MainCard.kt
+++ app/src/main/java/com/sddrozdov/weatherappcompose/ui/screens/MainCard.kt
@@ -1,16 +1,12 @@
 package com.sddrozdov.weatherappcompose.ui.screens
 
-import android.widget.TableLayout
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -22,10 +18,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -127,7 +121,9 @@ fun TabLayout() {
 
     val tabList = listOf("HOURS", "DAYS")
 
-    Column(modifier = Modifier.clip(RoundedCornerShape(4.dp))) {
+    Column(modifier = Modifier
+        .padding(start = 4.dp, end = 4.dp)
+        .clip(RoundedCornerShape(4.dp))) {
         TabRow(
             selectedTabIndex = 0,
             indicator = {},
