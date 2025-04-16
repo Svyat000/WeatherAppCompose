01f6bcca935c179ef17e81c8539015c9787a36cc
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-04-16 13:49:15 +0300

    add test model WeatherM

---
+++ app/src/main/java/com/sddrozdov/weatherappcompose/models/WeatherM.kt
@@ -0,0 +1,13 @@
+package com.sddrozdov.weatherappcompose.models
+
+data class WeatherM(
+    val city: String,
+    val time: String,
+    val currentTemp: String,
+    val condition: String,
+    val icon: String,
+    val tempMax: String,
+    val tempMix:String,
+    val hours: String,
+
+)
