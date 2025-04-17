bad5420aad939da8d5ea03067a14e82078c189df
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-04-17 19:04:08 +0300

    ref model weatherResponse and weatherApi

--- app/src/main/java/com/sddrozdov/weatherappcompose/api/WeatherApi.kt
+++ app/src/main/java/com/sddrozdov/weatherappcompose/api/WeatherApi.kt
@@ -8,6 +8,13 @@ interface WeatherApi {
     @GET("current.json")
     suspend fun getCurrentWeather(
         @Query("key") apiKey: String,
-        @Query("q") location: String
+        @Query("q") location: String,
+    ): WeatherResponse
+
+    @GET("forecast.json")
+    suspend fun getWeatherWithDays(
+        @Query("key") apiKey: String,
+        @Query("q") location: String,
+        @Query("days") days: Int
     ): WeatherResponse
 }
--- app/src/main/java/com/sddrozdov/weatherappcompose/models/WeatherResponse.kt
+++ app/src/main/java/com/sddrozdov/weatherappcompose/models/WeatherResponse.kt
@@ -2,5 +2,6 @@ package com.sddrozdov.weatherappcompose.models
 
 data class WeatherResponse(
     val location: Location,
-    val current: Current
+    val current: Current,
+    val forecast: Forecast
 )
