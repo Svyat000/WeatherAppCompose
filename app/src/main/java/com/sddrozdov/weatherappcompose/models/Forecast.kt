ded9556cc1f7acfc718ad9af360bd07b5f1ca647
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-04-17 18:12:46 +0300

    add Forecast Day

--- app/src/main/java/com/sddrozdov/weatherappcompose/models/Forecast.kt
+++ app/src/main/java/com/sddrozdov/weatherappcompose/models/Forecast.kt
@@ -5,3 +5,14 @@ import com.google.gson.annotations.SerializedName
 data class Forecast(
     @SerializedName("forecastday") val forecastDays: List<ForecastDay>
 )
+data class ForecastDay(
+    val date: String,
+    val day: Day,
+    val hour: List<Hour>
+)
+data class Day(
+    @SerializedName("maxtemp_c") val maxTempC: Double,
+    @SerializedName("mintemp_c") val minTempC: Double,
+    val condition: Condition,
+)
+
