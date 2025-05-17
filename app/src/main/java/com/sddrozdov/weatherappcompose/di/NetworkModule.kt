9f97b8895d6bdac71cd67534501f40de2dd66557
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-05-17 13:46:40 +0300

    add di retrofit and WeatherApi

---
+++ app/src/main/java/com/sddrozdov/weatherappcompose/di/NetworkModule.kt
@@ -0,0 +1,30 @@
+package com.sddrozdov.weatherappcompose.di
+
+import com.sddrozdov.weatherappcompose.data.api.WeatherApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://api.weatherapi.com/v1/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideWeatherApi(retrofit: Retrofit): WeatherApi =
+        retrofit.create(WeatherApi::class.java)
+
+}
